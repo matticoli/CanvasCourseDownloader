073abe73616e751c8cd2b87616d5557705301ca2
Benjamin Ward <benjaminloganward@gmail.com>
2021-01-13 01:10:08 -0500

    Remove extraneous files

--- firefox_test.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-url = "https://canvas.wpi.edu"
-
-if __name__ == "__main__":
-    webbrowser.register("firefox", None,
-                        webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
-
-    webbrowser.get("firefox").open_new_tab(url)
